5867b72da1164b1d1b70715fc2872c953ddf5077
jiaminglei <jiaminglei@blacklake.cn>
2022-10-31 14:48:29 +0800

    no1 two sum

--- src/No1_TwoSum/Solution.java
+++ src/No1_TwoSum/Solution.java
@@ -1,12 +1,29 @@
 package No1_TwoSum;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
 
     public static void main(String[] args) {
-        System.out.println("11111");
+        int[] nums = new int[]{3,2,4};
+        int target = 6;
+        int[] res = twoSum(nums, target);
+        for (int i = 0; i < res.length; i++) {
+            System.out.println(res[i]);
+        }
     }
 
-    public int[] twoSum(int[] nums, int target) {
-        return new int[1];
+    public static int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(nums[i]) != null) {
+                res[0] = map.get(nums[i]);
+                res[1] = i;
+            }
+            map.put(target - nums[i],i);
+        }
+        return res;
     }
 }

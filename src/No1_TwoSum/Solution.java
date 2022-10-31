5e72c487008589966cc0258fe6d1a715d36c49d5
jiaminglei <jiaminglei@blacklake.cn>
2022-10-31 13:57:34 +0800

    init

---
+++ src/No1_TwoSum/Solution.java
@@ -0,0 +1,12 @@
+package No1_TwoSum;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        return new int[1];
+    }
+}

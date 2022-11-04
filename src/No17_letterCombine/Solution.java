3078df4db6cacf726b3697490a010de491c3543d
jiaminglei <jiaminglei@blacklake.cn>
2022-11-04 17:28:14 +0800

    第17题：电话数字英文字母组合

--- src/No17_letterCombine/Solution.java
+++ src/No17_letterCombine/Solution.java
@@ -1,13 +1,11 @@
 package No17_letterCombine;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
 
     public static void main(String[] args) {
-        List<String> res = letterCombinations("234");
+        List<String> res = letterCombinations2("23");
         for (int i = 0; i < res.size(); i++) {
             System.out.println(res.get(i));
         }
@@ -45,4 +43,45 @@ public class Solution {
             }
         }
     }
+
+    public static List<String> letterCombinations2(String digits) {
+        HashMap<Character, String> hashMap = new HashMap();
+        hashMap.put('2', "abc");
+        hashMap.put('3', "def");
+        hashMap.put('4', "ghi");
+        hashMap.put('5', "jkl");
+        hashMap.put('6', "mno");
+        hashMap.put('7', "pqrs");
+        hashMap.put('8', "tuv");
+        hashMap.put('9', "wxyz");
+        List<String> res = new ArrayList<>();
+        Queue<String> queue = new LinkedList();
+        if (digits == null || digits.length() == 0) return res;
+
+        for (int i = 0; i < digits.length(); i++) {
+            String letter = hashMap.get(digits.charAt(i));
+            combineQueue(queue, letter);
+        }
+        for (String s : queue) {
+            res.add(s);
+        }
+        return res;
+    }
+    public static Queue<String> combineQueue(Queue<String> curQueue, String curLetters) {
+        if (curQueue.size() == 0) {
+            for (int i = 0; i < curLetters.length(); i++) {
+                curQueue.add(curLetters.charAt(i) + "");
+            }
+        } else {
+            int queueSize = curQueue.size();//这是最重要的一步，遍历queue的时候，一定要先把queue的sie记下来，不然在for循环中使用queue.size作为结束条件，会陷入死循环。
+            for (int i = 0; i < queueSize; i++) {
+                String top = curQueue.poll();
+                for (int j = 0; j < curLetters.length(); j++) {
+                    curQueue.add(top + curLetters.charAt(j));
+                }
+            }
+        }
+        return curQueue;
+    }
+
 }

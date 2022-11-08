5e6469219d4fb4b341fe5bbf0968bd0c43416a34
jiaminglei <jiaminglei@blacklake.cn>
2022-11-08 11:01:01 +0800

    第33题：难度加大版二分法

---
+++ src/No33_search/Solution.java
@@ -0,0 +1,45 @@
+package No33_search;
+
+public class Solution {
+
+    public static void main(String[] args) {
+//        int[] nums = new int[]{4, 5, 6, 7, 0, 1, 2};
+        int[] nums = new int[]{5, 1, 3};
+        int target = 3;
+        System.out.println(search(nums, target));
+    }
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && nums[mid] > target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}

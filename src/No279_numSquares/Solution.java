6520be8eabb69546cf9292903a21ce6b646832f1
jiaminglei <jiaminglei@blacklake.cn>
2022-11-28 15:15:15 +0800

    第279题：完全平方数

---
+++ src/No279_numSquares/Solution.java
@@ -0,0 +1,18 @@
+package No279_numSquares;
+
+public class Solution {
+
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        for (int i = 1; i < n+1; i++) {
+            int min = Integer.MAX_VALUE;
+            for (int j = 1; j * j <= i; j++) {
+                min = Math.min(min, dp[i - j * j]);
+            }
+            dp[i] = min + 1;
+        }
+        return dp[n];
+    }
+
+}

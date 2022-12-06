944fd72405c640b9896756057d69d2f0ad16bdf3
jiaminglei <jiaminglei@blacklake.cn>
2022-12-06 09:44:12 +0800

    第399题：不会

---
+++ src/No399_calcEquation/Solution.java
@@ -0,0 +1,12 @@
+package No399_calcEquation;
+
+import java.util.List;
+
+/**
+ * 不会，完全不会，不想会，面试出这题直接再见
+ */
+public class Solution {
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        return null;
+    }
+}

2fd70b1261fdd39ce1ee7ab006c8f05a3207273e
jiaminglei <jiaminglei@blacklake.cn>
2022-12-06 16:12:14 +0800

    第437题：路径总和 III

--- src/No437_pathSum/Solution.java
+++ src/No437_pathSum/Solution.java
@@ -2,6 +2,9 @@ package No437_pathSum;
 
 import Common.TreeNode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solution {
     /**
      * 深度优先 DFS
@@ -65,5 +68,37 @@ public class Solution {
         }
         dfsNode(root.left, target, sum);
         dfsNode(root.right, target, sum);
+    }
+
+
+    /**
+     * 前缀和
+     * @param root
+     * @param targetSum
+     * @return
+     */
+    public int pathSum3(TreeNode root, int targetSum) {
+        Map<Long, Integer> prefix = new HashMap<>();
+        prefix.put(0L, 1);
+        return dfs(root, prefix, 0, targetSum);
+    }
+
+    public int dfs(TreeNode root, Map<Long, Integer> prefix, long curSum, int targetSum) {
+        if (root == null) {
+            return 0;
+        }
+
+        int ret = 0;
+        curSum += root.val;
+
+        /**
+         * 回溯
+         */
+        ret = prefix.getOrDefault(curSum - targetSum, 0);
+        prefix.put(curSum, prefix.getOrDefault(curSum, 0) + 1);
+        ret += dfs(root.left, prefix, curSum, targetSum);
+        ret += dfs(root.right, prefix, curSum, targetSum);
+        prefix.put(curSum, prefix.getOrDefault(curSum, 0) - 1);
+        return ret;
     }
 }

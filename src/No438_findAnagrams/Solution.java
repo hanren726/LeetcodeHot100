bac273e95084eaf97507fe45979f707eb585d410
jiaminglei <jiaminglei@blacklake.cn>
2022-12-08 16:27:03 +0800

    第438题：找到字符串中所有字母异位词

--- src/No438_findAnagrams/Solution.java
+++ src/No438_findAnagrams/Solution.java
@@ -1,13 +1,53 @@
 package No438_findAnagrams;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "cbaebabacd";
+        String p = "abc";
+        System.out.println(solution.findAnagrams(s, p));
+    }
     public List<Integer> findAnagrams(String s, String p) {
 
-        return new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
 
+        Map<Character, Integer> need = new HashMap<>();
+        Map<Character, Integer> window = new HashMap<>();
+
+        int left = 0;
+        int right = 0;
+        int valid = 0;
+        for (char pChar : p.toCharArray()) {
+            need.put(pChar, need.getOrDefault(pChar, 0) + 1);
+        }
+        while (right < s.length()) {
+            char cur = s.charAt(right++);
+            //加入窗口时，先加window，再比较更新valid
+            if (need.get(cur) != null) {
+                window.put(cur, window.getOrDefault(cur, 0) + 1);
+                if (window.get(cur).intValue() == need.get(cur).intValue()) {
+                    valid++;
+                }
+            }
+            while (right - left >= p.length()) {
+                //当right-left大于p的长度时，可以开始收缩窗口了
+                if (valid == need.size()) {
+                    res.add(left);
+                }
+                char d = s.charAt(left);
+                left++;
+                //缩小窗口时，先比较更新valid，再移除window
+                if (need.get(d) != null) {
+                    if (window.get(d).intValue() == need.get(d).intValue()) {
+                        valid--;
+                    }
+                    window.put(d, window.getOrDefault(d, 0) - 1);
+                }
+            }
+        }
+        return res;
     }
 }
